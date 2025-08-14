6292bb71d9e28f11ae221a54322fb14a1531deab
divyanshgoyal05@gmail.com <divyanshgoyal05@gmail.com>
2025-08-15 01:58:55 +0530

    changed application properties for railway deployment

--- src/main/java/com/example/cloudbalanced/CloudbalancedApplication.java
+++ src/main/java/com/example/cloudbalanced/CloudbalancedApplication.java
@@ -3,7 +3,7 @@ package com.example.cloudbalanced;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication 
+@SpringBootApplication
 public class CloudbalancedApplication {
 
 	public static void main(String[] args) {
